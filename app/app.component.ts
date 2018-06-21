03d6d1ed4395c4798d086a069bd98e4f7372f91b
vishal <vishalsharma25sept@gmail.com>
2018-06-21 14:35:49 +0530

    Initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<div style="text-align:center">
+  <h1>
+    Welcome to Marvellous Infosystems!!!
+  </h1>
+  <br>
+
+  <h2>“Educating for better tomorrow”.</h2>
+</div>`,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = '';
+}
